175c0a4d6a9c3b22e1ea4311987b209a79335e73
Michelle Beshears <meeshysu@gmail.com>
2018-10-01 14:46:30 -0500

    added photo to book

--- components/store.js
+++ components/store.js
@@ -5,17 +5,19 @@ import {printToDom} from '../helpers/util.js'
 const addToCart = document.getElementById('cart');
 
 const bookInfo = {
-    photo: "",
-    name: "Counter-Strike: Standing On Your Mate's Head",
+    photo: "Boosting.png",
+    name: "Counter-Strike: The Art of Boosting",
+    author: "by Alex 'Mangos' Kallman",
     price: "20.99"
 };
 
 
 const bookInfoStringBuilder = () => {
         let newString = '';
-        newString += `<div class="card w-25">`;
-        newString +=    `<h2>${bookInfo.photo}</h2>`;
+        newString += `<div class="card w-25 p-4 m-4">`;
+        newString +=    `<img src="${bookInfo.photo}"></img>`;
         newString +=    `<h4>${bookInfo.name}</h4>`;
+        newString +=    `<p>${bookInfo.author}</p>`;
         newString +=    `<p>${bookInfo.price}</p>`
         newString += `</div>`;
         printToDom(newString, 'bookInfoSpace');
